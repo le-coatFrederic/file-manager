7ed10b98d7837cb701041f5d0b5ae2039b182c2c
Frédéric LE COAT <lecoatfred@gmail.com>
2025-10-28 13:35:14 +0100

    retirer les logs inutiles

--- src/main/java/com/fredlecoat/file_manager/services/FileScannerService.java
+++ src/main/java/com/fredlecoat/file_manager/services/FileScannerService.java
@@ -34,20 +34,14 @@ public class FileScannerService {
         this.processCurrentFile(inputDir, null);
     }
 
-    private void processCurrentFile(File folder, FileEntity parentFolder) { 
-        System.out.println("FileEntity creation.");       
+    private void processCurrentFile(File folder, FileEntity parentFolder) {     
         FileEntity newFile = new FileEntity(
             folder.getName(), 
             folder.isDirectory() == true ? FileType.FOLDER : FileType.FILE, 
             100L,
             LocalDateTime.ofInstant(Instant.ofEpochMilli(folder.lastModified()), ZoneId.systemDefault())
         );
-        
-        System.out.println("FileEntity created.");       
-
-        System.out.println("Setting parent.");  
         newFile.setParent_folder(parentFolder != null ? parentFolder : null);
-        System.out.println("Parent setted.");  
 
         this.fileRepository.save(newFile);
 
